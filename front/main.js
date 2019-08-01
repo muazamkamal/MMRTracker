061d9485e886396117f6513d5e2e6860186f696e
Muazam Kamal <muazam.kamal@gmail.com>
2019-08-01 19:12:59 +0800

    Implemented dummy front-end (electron).

---
+++ front/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow()
+{
+    win = new BrowserWindow({
+        width: 600,
+        height: 200,
+        resizable: false,
+        fullscreenable: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html')
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
