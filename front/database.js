43aa8ba7675f0054d6d041b5708d580869b31fe1
Muazam Kamal <muazam.kamal@gmail.com>
2019-08-13 03:13:15 +0800

    Added database checking, and showing MMR to front.

--- front/database.js
+++ front/database.js
@@ -1,9 +1,40 @@
-var fs = require('fs')
-var path = require('path')
-var initSqlJs = require('./sql-wasm')
-var filebuffer = fs.readFileSync(path.join(__dirname, '../back/s3_1_beta.db'))
+const fs = require('fs')
+const path = require('path')
+const initSqlJs = require('./sql-wasm')
+const { ipcRenderer } = require('electron')
+
+function first () {
+  let exist = false
+
+  if (fs.existsSync(path.join(__dirname, '../back/s3_1_beta.db'))) {
+    exist = true
+  }
+
+  ipcRenderer.send('database-exist', exist)
+}
+
+function display (result) {
+  var core = document.getElementById('core')
+  var support = document.getElementById('support')
+
+  var coreText = core.innerHTML + result.core
+  var supporText = support.innerHTML + result.support
+
+  if (result.core === 'TBD') {
+    coreText = coreText + ', ' + result.coreremaining + ' games remaining.'
+  }
+
+  if (result.support === 'TBD') {
+    supporText = supporText + ', ' + result.supportremaining + ' games remaining.'
+  }
+
+  core.innerHTML = coreText
+  support.innerHTML = supporText
+}
 
 function read () {
+  const filebuffer = fs.readFileSync(path.join(__dirname, '../back/s3_1_beta.db'))
+
   initSqlJs().then(SQL => {
     // Create a database
     var db = new SQL.Database(filebuffer)
@@ -11,8 +42,10 @@ function read () {
     // data is an Uint8Array representing an SQLite database file
 
     // Prepare an sql statement
-    db.each('SELECT * FROM mmr ORDER BY time DESC LIMIT 1',
-      function (row) { console.log(row.time + ': ' + row.core) }
-    )
+    db.each('SELECT * FROM mmr ORDER BY time DESC LIMIT 1', (row) => {
+      display(row)
+    })
+
+    db.close()
   })
 }
--- front/main.js
+++ front/main.js
@@ -1,9 +1,24 @@
-const { app, BrowserWindow } = require('electron')
-
-let win
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 function createWindow () {
-  win = new BrowserWindow({
+  // Main window
+  let main = new BrowserWindow({
+    width: 600,
+    height: 200,
+    resizable: true,
+    fullscreenable: false,
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  main.on('closed', () => {
+    main = null
+  })
+
+  // Loading window
+  let loading = new BrowserWindow({
     width: 600,
     height: 200,
     resizable: true,
@@ -13,10 +28,27 @@ function createWindow () {
     }
   })
 
-  win.loadFile('index.html')
+  loading.loadFile('loading.html')
+  // loading.webContents.openDevTools()
 
-  win.on('closed', () => {
-    win = null
+  loading.on('closed', () => {
+    loading = null
+  })
+
+  ipcMain.once('database-exist', (event, status) => {
+    if (status) {
+      main.loadFile('index.html')
+    } else {
+      main.loadFile('first-setup.html')
+    }
+
+    loading.destroy()
+    loading = null
+
+    // main.webContents.openDevTools()
+    main.once('ready-to-show', () => {
+      main.show()
+    })
   })
 }
 
