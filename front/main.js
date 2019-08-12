eba31192fbef52bf9534858e824203be552f8f8d
Muazam Kamal <muazam.kamal@gmail.com>
2019-08-12 23:18:10 +0800

    Updated style guide to use Standard JS.

--- front/main.js
+++ front/main.js
@@ -2,23 +2,22 @@ const { app, BrowserWindow } = require('electron')
 
 let win
 
-function createWindow()
-{
-    win = new BrowserWindow({
-        width: 600,
-        height: 200,
-        resizable: true,
-        fullscreenable: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+function createWindow () {
+  win = new BrowserWindow({
+    width: 600,
+    height: 200,
+    resizable: true,
+    fullscreenable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-    win.loadFile('index.html')
+  win.loadFile('index.html')
 
-    win.on('closed', () => {
-        win = null
-    })
+  win.on('closed', () => {
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
